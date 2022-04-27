5106d6b9a8004ef7055051caa8ee5379ab44525c
jeya-maria-jose <jeyamariajose7@gmail.com>
2022-04-27 13:12:58 -0400

    added UNexT-S and OG UNext

--- archs.py
+++ archs.py
@@ -207,6 +207,140 @@ class UNext(nn.Module):
 
     ## Conv 3 + MLP 2 + shifted MLP
     
+    def __init__(self,  num_classes, input_channels=3, deep_supervision=False,img_size=224, patch_size=16, in_chans=3,  embed_dims=[ 128, 160, 256],
+                 num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
+                 attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
+                 depths=[1, 1, 1], sr_ratios=[8, 4, 2, 1], **kwargs):
+        super().__init__()
+        
+        self.encoder1 = nn.Conv2d(3, 16, 3, stride=1, padding=1)  
+        self.encoder2 = nn.Conv2d(16, 32, 3, stride=1, padding=1)  
+        self.encoder3 = nn.Conv2d(32, 128, 3, stride=1, padding=1)
+
+        self.ebn1 = nn.BatchNorm2d(16)
+        self.ebn2 = nn.BatchNorm2d(32)
+        self.ebn3 = nn.BatchNorm2d(128)
+        
+        self.norm3 = norm_layer(embed_dims[1])
+        self.norm4 = norm_layer(embed_dims[2])
+
+        self.dnorm3 = norm_layer(160)
+        self.dnorm4 = norm_layer(128)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+
+        self.block1 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.block2 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[2], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.dblock1 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.dblock2 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[0], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],
+                                              embed_dim=embed_dims[1])
+        self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=3, stride=2, in_chans=embed_dims[1],
+                                              embed_dim=embed_dims[2])
+
+        self.decoder1 = nn.Conv2d(256, 160, 3, stride=1,padding=1)  
+        self.decoder2 =   nn.Conv2d(160, 128, 3, stride=1, padding=1)  
+        self.decoder3 =   nn.Conv2d(128, 32, 3, stride=1, padding=1) 
+        self.decoder4 =   nn.Conv2d(32, 16, 3, stride=1, padding=1)
+        self.decoder5 =   nn.Conv2d(16, 16, 3, stride=1, padding=1)
+
+        self.dbn1 = nn.BatchNorm2d(160)
+        self.dbn2 = nn.BatchNorm2d(128)
+        self.dbn3 = nn.BatchNorm2d(32)
+        self.dbn4 = nn.BatchNorm2d(16)
+        
+        self.final = nn.Conv2d(16, num_classes, kernel_size=1)
+
+        self.soft = nn.Softmax(dim =1)
+
+    def forward(self, x):
+        
+        B = x.shape[0]
+        ### Encoder
+        ### Conv Stage
+
+        ### Stage 1
+        out = F.relu(F.max_pool2d(self.ebn1(self.encoder1(x)),2,2))
+        t1 = out
+        ### Stage 2
+        out = F.relu(F.max_pool2d(self.ebn2(self.encoder2(out)),2,2))
+        t2 = out
+        ### Stage 3
+        out = F.relu(F.max_pool2d(self.ebn3(self.encoder3(out)),2,2))
+        t3 = out
+
+        ### Tokenized MLP Stage
+        ### Stage 4
+
+        out,H,W = self.patch_embed3(out)
+        for i, blk in enumerate(self.block1):
+            out = blk(out, H, W)
+        out = self.norm3(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        t4 = out
+
+        ### Bottleneck
+
+        out ,H,W= self.patch_embed4(out)
+        for i, blk in enumerate(self.block2):
+            out = blk(out, H, W)
+        out = self.norm4(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        ### Stage 4
+
+        out = F.relu(F.interpolate(self.dbn1(self.decoder1(out)),scale_factor=(2,2),mode ='bilinear'))
+        
+        out = torch.add(out,t4)
+        _,_,H,W = out.shape
+        out = out.flatten(2).transpose(1,2)
+        for i, blk in enumerate(self.dblock1):
+            out = blk(out, H, W)
+
+        ### Stage 3
+        
+        out = self.dnorm3(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        out = F.relu(F.interpolate(self.dbn2(self.decoder2(out)),scale_factor=(2,2),mode ='bilinear'))
+        out = torch.add(out,t3)
+        _,_,H,W = out.shape
+        out = out.flatten(2).transpose(1,2)
+        
+        for i, blk in enumerate(self.dblock2):
+            out = blk(out, H, W)
+
+        out = self.dnorm4(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        out = F.relu(F.interpolate(self.dbn3(self.decoder3(out)),scale_factor=(2,2),mode ='bilinear'))
+        out = torch.add(out,t2)
+        out = F.relu(F.interpolate(self.dbn4(self.decoder4(out)),scale_factor=(2,2),mode ='bilinear'))
+        out = torch.add(out,t1)
+        out = F.relu(F.interpolate(self.decoder5(out),scale_factor=(2,2),mode ='bilinear'))
+
+        return self.final(out)
+
+
+class UNext_S(nn.Module):
+
+    ## Conv 3 + MLP 2 + shifted MLP w less parameters
+    
     def __init__(self,  num_classes, input_channels=3, deep_supervision=False,img_size=224, patch_size=16, in_chans=3,  embed_dims=[32, 64, 128, 512],
                  num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
                  attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
